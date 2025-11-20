8153782784a2b1c61e0f870a49630e27d843907b
Dariusz Okoński <darek200180@gmail.com>
2025-11-20 08:24:57 +0100

    hide session secret in env file

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   session({
-    secret: 'xyz567',
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
     store: MongoStore.create({
