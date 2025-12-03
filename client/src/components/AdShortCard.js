aff5ce972e512b1f8566167d1324e95925e75eb1
Dariusz Okoński <darek200180@gmail.com>
2025-12-03 06:32:14 +0100

    change AdShortCard to be displayed in 3 columns

--- client/src/components/AdShortCard.js
+++ client/src/components/AdShortCard.js
@@ -1,30 +1,55 @@
 import React from 'react';
-import { Card, Button } from 'react-bootstrap';
+import { Card, Button, Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
-const AdShortCard = ({ id, title, picture, location }) => (
-  <Card className='h-100'>
-    {picture && (
-      <Card.Img
-        variant='top'
-        // src={`../../../public/uploads/${picture}`}
-        src=''
-        alt={title}
-        style={{ objectFit: 'cover', height: 200 }}
-      />
-    )}
-    <Card.Body className='d-flex flex-column'>
-      <Card.Title>{title}</Card.Title>
-      <Card.Text className='text-muted small'>
-        {location || 'Location not specified'}
-      </Card.Text>
-      <div className='mt-auto'>
-        <Button as={Link} to={`/ads/${id}`} variant='primary' className='w-100'>
-          Read More
-        </Button>
-      </div>
-    </Card.Body>
-  </Card>
-);
+const AdShortCard = ({ id, title, picture, location }) => {
+  // TODO: fix src later on
+  const src = picture
+    ? picture.startsWith('http')
+      ? picture
+      : `/uploads/${picture}`
+    : null;
+
+  return (
+    <Card className='w-100 mb-3'>
+      <Card.Body>
+        <Row className='align-items-center'>
+          <Col xs={12} md={4} lg={4} className='mb-2 mb-md-0'>
+            {src ? (
+              <img
+                src={src}
+                alt={title}
+                className='img-fluid rounded'
+                style={{ width: '100%', height: '140px', objectFit: 'cover' }}
+              />
+            ) : (
+              <div
+                style={{
+                  width: '100%',
+                  height: 140,
+                  backgroundColor: '#e9ecef',
+                  borderRadius: 6,
+                }}
+              />
+            )}
+          </Col>
+
+          <Col xs={12} md={5} lg={6} className='mb-2 mb-md-0'>
+            <h5 className='mb-1'>{title}</h5>
+            <p className='text-muted mb-0'>
+              {location || 'Location not specified'}
+            </p>
+          </Col>
+
+          <Col xs={12} md={3} lg={2} className='text-md-end mt-3 mt-md-0'>
+            <Button as={Link} to={`/ads/${id}`} variant='primary'>
+              Read More
+            </Button>
+          </Col>
+        </Row>
+      </Card.Body>
+    </Card>
+  );
+};
 
 export default AdShortCard;
